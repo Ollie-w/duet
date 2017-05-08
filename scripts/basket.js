9e8e4269daa08ca3dc197547093076a916b22e2f
web.developer <Web.Developer@CHAMBERLAINMUSIC.COM>
2017-05-08 10:31:25 +0100

    added chained transitions when product is removed from basket

--- scripts/basket.js
+++ scripts/basket.js
@@ -1,32 +1,22 @@
-var removeButtons = Array.from(document.querySelectorAll(".remove-btn"));
 var products = document.querySelectorAll(".product");
-
-/*
-var products = Array.from(document.querySelectorAll(".product"));
-
-products.forEach(function(element) {
-element.addEventListener("click", function(e) {
-    console.log(this);
-if (e.target.classList.contains("remove-btn")) {
-    $(this).remove();
-}    
-})
-})
-
-
-$(".product").on("click", ".remove-btn", function() {
-     $(this).remove();
-}) 
-
 for (let i = 0; i < products.length; i++) {
 products[i].addEventListener("click", function(e) {
     if (e.target.classList.contains("remove-btn")) {
-        console.log(this);
-    $(this).remove();
+        this.classList.add("vanish");
 }
 })
-}*/
-
-$(".remove-btn").on("click", function() {
-    $(this).closest(".product").remove();
-})
+} 
+for (let i = 0; i < products.length; i++) {
+products[i].addEventListener("transitionend", function(event) {
+  if (event.propertyName === "opacity") {
+      this.classList.add("collapse");
+  }
+})  
+}
+for (let i = 0; i < products.length; i++) {
+products[i].addEventListener("transitionend", function(event) {
+  if (event.propertyName === "max-height") {
+      this.remove();
+  }
+})  
+}
